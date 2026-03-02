ccbb3337122ade1aee8b62c2081733fa0316688e
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2026-03-02 17:03:34 -0300

    User entity class created (minor adjustments may be needed as the project grows). Also, H2 configured till DB is running.

---
+++ backend/src/main/java/com/tcghub/backend/model/User.java
@@ -0,0 +1,17 @@
+package com.tcghub.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+}
---
+++ backend/src/main/java/com/tcghub/backend/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.tcghub.backend.repository;
+
+public class UserRepository {
+
+}
