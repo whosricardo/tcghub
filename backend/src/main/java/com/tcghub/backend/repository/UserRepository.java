7551066dc9430da391afe5b4b701b0c34acc72c4
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2026-03-02 17:33:58 -0300

    created UserRepository save method

--- backend/src/main/java/com/tcghub/backend/repository/UserRepository.java
+++ backend/src/main/java/com/tcghub/backend/repository/UserRepository.java
@@ -1,5 +1,21 @@
 package com.tcghub.backend.repository;
 
-public class UserRepository {
+import com.tcghub.backend.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class UserRepository {
+    private final JdbcTemplate jTemplate;
+
+    @Autowired
+    public UserRepository(JdbcTemplate jTemplate) {
+        this.jTemplate = jTemplate;
+    }
+
+    public void save(User user) {
+        String sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        jTemplate.update(sql, user.getUsername(), user.getEmail(), user.getPassword());
+    }
 }
