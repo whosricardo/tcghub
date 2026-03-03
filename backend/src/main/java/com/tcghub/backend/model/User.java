b2d47a59fe43763ff2b24925d8410e7a636df5aa
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2026-03-03 16:51:19 -0300

    adjusted user entity

--- backend/src/main/java/com/tcghub/backend/model/User.java
+++ backend/src/main/java/com/tcghub/backend/model/User.java
@@ -1,5 +1,11 @@
 package com.tcghub.backend.model;
 
+import java.util.List;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +15,44 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     private Long id;
     private String username;
     private String email;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
