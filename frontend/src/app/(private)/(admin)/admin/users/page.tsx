f74a484334cf3b490a8a237e786b265dea5241e4
Lucasmenezes08 <lms4@cesar.school>
2026-03-20 14:26:32 -0300

    :lipstick: style: edit userTable style

--- frontend/src/app/(private)/(admin)/admin/users/page.tsx
+++ frontend/src/app/(private)/(admin)/admin/users/page.tsx
@@ -2,10 +2,10 @@ import { UserTable } from "@/components/admin/layout/user-table";
 
 export default function Users (){
     return (
-        <section className="p-8 space-y-6">
+        <section className="p-8 space-y-10">
             <header className="flex flex-col w-full">
-                <h1>Gestão de usuários</h1>
-                <h2>Visualize e gerencie os usuário do sistema</h2>
+                <h1 className="text-2xl font-bold text-black">Gestão de usuários</h1>
+                <h2 className="text-md text-gray-400 break-after-auto">Visualize e gerencie os usuário do sistema</h2>
             </header>
 
             <section className="w-full flex justify-center items-center">
