940fe15ddb5b0f0ede3ae50520b4d1a3d0bde641
Lucasmenezes08 <lms4@cesar.school>
2026-03-30 08:11:57 -0300

    :bug: fix: Edit tableSkeleton error

--- frontend/src/shared/table-skeleton.tsx
+++ frontend/src/shared/table-skeleton.tsx
@@ -1,13 +1,18 @@
-import { Skeleton } from "@/components/ui/skeleton";
+import { TableCell, TableRow } from '@/components/ui/table'
+import { Skeleton } from '@/components/ui/skeleton' 
 
-export default function TableSkeleton (){
+export default function TableSkeleton() {
     return (
-        <section className="flex w-full items-center justify-start gap-6">
-            <Skeleton className="h-16 w-16 rounded-2xl shrink-0"/>
-            <section className="flex flex-col gap-2 flex-1">
-                <Skeleton className="h-3 w-3/4"/>
-                <Skeleton className="h-3 w-1/2"/>
-            </section>
-        </section>
+        <TableRow>
+            <TableCell>
+                <Skeleton className="h-4 w-12" /> 
+            </TableCell>
+            <TableCell>
+                <Skeleton className="h-4 w-32" />
+            </TableCell>
+            <TableCell>
+                <Skeleton className="h-4 w-48" /> 
+            </TableCell>
+        </TableRow>
     )
 }
