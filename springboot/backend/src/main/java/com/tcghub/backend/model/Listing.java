33fa9944a808fe6700e244b41e92975db3670b42
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2026-04-01 22:57:21 -0300

    implemented list data model

---
+++ springboot/backend/src/main/java/com/tcghub/backend/model/Listing.java
@@ -0,0 +1,85 @@
+package com.tcghub.backend.model;
+
+import java.math.BigDecimal;
+
+public class Listing {
+    private Long id;
+    private Integer availableQuantity;
+    private BigDecimal currentPrice;
+    private String itemCondition;
+    private String productLanguage;
+    private Long productId;
+    private Long supplierId;
+
+    public Listing() {
+    }
+
+    public Listing(
+            Long id,
+            Integer availableQuantity,
+            BigDecimal currentPrice,
+            String itemCondition,
+            String productLanguage,
+            Long productId,
+            Long supplierId) {
+        this.id = id;
+        this.availableQuantity = availableQuantity;
+        this.currentPrice = currentPrice;
+        this.itemCondition = itemCondition;
+        this.productLanguage = productLanguage;
+        this.productId = productId;
+        this.supplierId = supplierId;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getAvailableQuantity() {
+        return this.availableQuantity;
+    }
+
+    public void setAvailableQuantity(Integer availableQuantity) {
+        this.availableQuantity = availableQuantity;
+    }
+
+    public BigDecimal getCurrentPrice() {
+        return this.currentPrice;
+    }
+
+    public void setCurrentPrice(BigDecimal currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public String getItemCondition() {
+        return this.itemCondition;
+    }
+
+    public void setItemCondition(String itemCondition) {
+        this.itemCondition = itemCondition;
+    }
+
+    public String getProductLanguage() {
+        return this.productLanguage;
+    }
+
+    public void setProductLanguage(String productLanguage) {
+        this.productLanguage = productLanguage;
+    }
+
+    public Long getProductId() {
+        return this.productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getSupplierId(Long supplierId) {
+        return this.supplierId;
+    }
+}
