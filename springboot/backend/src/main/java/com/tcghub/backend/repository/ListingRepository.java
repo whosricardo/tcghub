dca0577aae10c512aef7e5998854bd30e77581ad
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2026-04-01 23:06:03 -0300

    add method to find listing by id

--- springboot/backend/src/main/java/com/tcghub/backend/repository/ListingRepository.java
+++ springboot/backend/src/main/java/com/tcghub/backend/repository/ListingRepository.java
@@ -1,8 +1,32 @@
 package com.tcghub.backend.repository;
 
+import java.util.Optional;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import com.tcghub.backend.model.Listing;
 
 @Repository
 public class ListingRepository {
+    private final JdbcTemplate jdbcTemplate;
 
+    public ListingRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<Listing> listingRowMapper = (rs, rowNum) -> new Listing(
+            rs.getLong("id"),
+            rs.getInt("available_quantity"),
+            rs.getBigDecimal("current_price"),
+            rs.getString("item_condition"),
+            rs.getString("product_language"),
+            rs.getLong("product_id"),
+            rs.getLong("supplier_id"));
+
+    public Optional<Listing> findById(Long id) {
+        String sql = "SELECT * FROM listings WHERE id = ?";
+        return jdbcTemplate.query(sql, listingRowMapper, id).stream().findFirst();
+    }
 }
